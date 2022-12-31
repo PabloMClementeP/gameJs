33480a4628d8d762d0374e5937bdb5efb2702238
Pablo Clemente <pclementep@gmail.com>
2022-12-30 23:44:33 -0300

    Canvas size, structure Js Class for gameObjects

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
 
 window.addEventListener("load", ()=>{
 
-    
+    // canvas setup
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500
+
+
+    class InputHandler
+    {
+
+    }
+
+    class Projectile
+    {
+
+    }
+
+    class Particle
+    {
+
+    }
+
+    class Player
+    {
+
+    }
+
+    class Enemy
+    {
+
+    }
+
+    class Layer
+    {
+
+    }
+
+    class Background
+    {
+
+    }
+
+    class UI
+    {
+
+    }
+
+    class Game
+    {
+        
+    }
 
 });
